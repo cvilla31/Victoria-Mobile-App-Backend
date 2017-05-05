9714e51d5e2d3f6a09d5556445aa8c4d48f977c2
Uxor IT B7 <Uxor IT B7@192.168.1.128>
2017-05-05 12:29:11 -0300

    Ajustes para mejorar legibilidad del código. Eliminación de archivos de configuració innecesarios.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# HelloTodo 
+# Mobile App Backend
 
 ## Overview
-HelloTodo is a simple todo list application created with [StrongLoop](https://strongloop.com/), [LoopBack](https://strongloop.com/node-js/loopback-framework/) and [Bluemix Mobile Services](http://www.ibm.com/cloud-computing/bluemix/solutions/mobilefirst/). It serves as a sample app demonstrating how to use the above technologies in order to create a mobile backend, enable authentication and collect monitoring data. 
+This Mobile App Backend for Frontend integrates Mobile Apps with the CORE Company Backend.
+It serves as a sample app demonstrating how to use the above technologies in order to create a mobile backend, enable authentication and access Core Backend Services. 
 
 ### Table of content
 * Overview
@@ -11,20 +12,8 @@ HelloTodo is a simple todo list application created with [StrongLoop](https://st
 * Manually deploying to Bluemix
 	* Getting setup with Bluemix
 	* Creating an instance of Mobile Client Access service
-	* Cloning and deploying HelloTodo app
-* Using the HelloTodo application and API
 
-## Creating this application 
-
-Below video demonstrates how the HelloTodo application was created. It might be instrumental in understanding how does StrongLoop, LoopBack and Mobile Client Access work. If you're just interested in running the app in Bluemix you may skip this video. 
-
-<a href="https://www.youtube.com/watch?v=fAM0wOfvelY" target="_blank">
-<img src="video-img.png"/>
-</a>
-
-
-## Deploying HelloTodo application to Bluemix
-
+## Deploying Mobile App Backend application to Bluemix
 There are two ways of running this application - using a MobileFirst Services Boilerplate on Bluemix or cloning the repository and deploying to Bluemix manually. 
 
 ## Using a MobileFirst Services Boilerplate
@@ -39,14 +28,6 @@ Start by creating a mobile backend on Bluemix by using the MobileFirst Services 
 5. Enter the name and a host for your mobile backend. 
 6. Optionally you can change service plans
 7. Click CREATE button
-
-As a result of the above steps Bluemix will provision a Node.JS runtime and populate it with a HelloTodo application created using StrongLoop. This application uses LoopBack framework to expose the `/api/Items` API which will be used by Web UI. This reposotory contains the source code of that HelloTodo application.
-
-> Some of the methods of the above API are protected by Mobile Client Access service, therefore they will be unaccessible from web UI. You will need to use mobile HelloTodo samples to be able to access these APIs. This will be explained later.
-
-Usually it might take couple of minutes for your application to be provisioned and started. Once you're back to your Bluemix Application Dashboard click on an application route found in the top part of a screen. 
-
-Once you open the web interface of a HelloTodo app you may continue to the `Using the HelloTodo application` and API section of this tutorial below
 
 ## Manually deploying to Bluemix
 To manually deploy this application to Bluemix perform the following steps
@@ -82,33 +63,3 @@ To manually deploy this application to Bluemix perform the following steps
 	> You can pick any other name instead of my-MCA-service-instane
 	
 1. Run `cf services` command and validate a new service instance was added
-
-### Cloning and deploying HelloTodo app
-
-1. Clone this repository to your local disk
-
-	```Shell
-	git clone https://github.com/ibm-bluemix-mobile-services/bms-hellotodo-strongloop
-	```
-	
-1. Edit `manifest.yml` file. Change the `host` property to some unique value. There might be other applications deployed to Bluemix with `hellotodo-with-strongloop` host. If do not change it your deployment might fail. 
-
-1. Edit `manifest.yml` file and add the services section. Add the Mobile Client Access service instance name created in previous steps. Final result should look like this
-
-	![image](updated-manifest.png) 
-
-1. Use `cf push` command to deploy your application to Bluemix, bind it to Mobile Client Access service instance and start it. 
-
-1. Once deployment completes successfully use `cf apps` command to see the list of available applications and their routes
-
-1. Your Bluemix application should be available at `https//{hostname-from-manifest.yml}.mybluemix.net`
-
-## Using the HelloTodo application and API
-You will see a HelloTodo web app. Start by following the guided experience steps described in the web UI. Eventually you will try to DELETE a todo item and will discover that this action can only be complete when using the HelloTodo mobile apps samples. This is due to a fact that the mobile backend is by default protected by a Mobile Client Access - a Bluemix service that provides security and monitoring functionality for mobile backends. 
-
-> You might also want to hit the "View API Reference" button on web UI to see the API specs.
-
-Continue experiencing Bluemix Mobile Services by using one of the following sample applications.
-
-* [HelloTodo for iOS](https://github.com/ibm-bluemix-mobile-services/bms-samples-ios-hellotodo)
-* [HelloTodo for Android](https://github.com/ibm-bluemix-mobile-services/bms-samples-android-hellotodo) 
--- server/server.js
+++ server/server.js
@@ -1,44 +1,119 @@
 var packageVersion = require('./../package.json').version;
+
 console.log("Victoria-Mobile-App-Backend v" + packageVersion);
 
 var express = require("express");
+var expressSession = require("express-session");
+var FileStore = require('session-file-store')(expressSession);
 var cfenv = require("cfenv");
 var bodyParser = require('body-parser');
 var iconv = require('iconv-lite');
-
+var nconf = require("nconf");
+var vcapLocal = require('./vcap-local.json'); //Configuration for local
+var VCAP_SERVICES = "VCAP_SERVICES"; //Configuration form Bluemix Services
+const vcapServices = JSON.parse(process.env[VCAP_SERVICES] || JSON.stringify(vcapLocal));
+const HOST = 'uxorlab.uxorit.com';
+const PORT = 10039; //FIXME Use 10029 for production
 var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-// ---------------------------- Unprotected mobile backend --------------------------------
+var store = new FileStore();
 var querystring = require('querystring');
+var http = require("http"); //FIXME Use https for production
 
-//var https = require('https');
-//var host = 'uxorlab.uxorit.com:10029/wps/PA_CEA-Admin/services';
-var http = require("http");
-var host = 'uxorlab.uxorit.com';
+app.use(expressSession({
+  store , 
+  secret: vcapServices.AdvancedMobileAccess[0].credentials.secret, 
+  cookie: { maxAge: 10000 }, 
+  saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /** 
- * Middleware API Mobile Backend
+ * Autenticación con Backend
  * Victoria-Mobile-App -> Victoria-Mobile-App-Backend 
  * 
  * @param request: Solicitud desde Victoria-Mobile-App
  * @param response: Respuesta a Victoria-Mobile-App
  */
-app.get("/api/v1/autos/*", function (request, response) {
-  backendService(request, function(status, result) { response.statusCode = status; response.send(result); } );
+app.get("/api/v1/login", function (request, response) {
+  var onResult = function(status, result) {
+      response.statusCode = status;
+      response.send(result);
+  };
+  
+  var usuario = request.query.userid;
+  var clave = request.query.password;
+  
+  var options = {
+    host: HOST, 
+    port: PORT, 
+    path: '/wps/portal/cxml/04_SD9ePMtCP1I800I_KydQvyHFUBADPmuQy?userid=' + usuario + '&password=' + clave,
+    method: 'GET'
+  };
+
+  var req = http.request(options, function(res)
+  {
+    var output;
+    console.log(options.host + ':' + res.statusCode);
+    
+    switch (res.statusCode) {
+    case 302:
+      store.set(request.session.id, request.session, function(err) {
+        if (err) {
+          res.statusCode = 401;
+          output = {"authenticated": false};
+          onResult(res.statusCode, output);
+        } else {
+          res.statusCode = 200;
+          output = {"authenticated": true, "token":request.session.id};
+          onResult(res.statusCode, output);
+        }
+      });
+      break;
+    case 404:
+      res.statusCode = 401;
+      output =  {"authenticated": false};
+      onResult(res.statusCode, output);
+      break;
+    default:
+      res.statusCode = 401;
+      output = {"error": "Error al consumir servicio de Backend", "authenticated": false};
+      onResult(res.statusCode, output);
+      break;
+    }
+  });
+
+  req.on('error', function(err) {
+    onResult('500', {'error': 'Error al consumir servicio de Backend', "authenticated": false});
+  });
+
+  req.end();
 });
 
 /** 
  * Middleware API Mobile Backend
  * Victoria-Mobile-App -> Victoria-Mobile-App-Backend 
  * 
- * @param request: Solicitud desde Victoria-Mobile-App
- * @param response: Respuesta a Victoria-Mobile-App
+ * @param req: Solicitud desde Victoria-Mobile-App
+ * @param res: Respuesta a Victoria-Mobile-App
  */
-app.post("/api/v1/autos/*", function (request, response) {
-  backendService(request, function(status, result) { response.statusCode = status; response.send(result); } );
+app.get("/api/v1/autos/*", function (req, res) {
+  var sess = req.session;
+  var tokenId = req.query.tokenId;
+  
+  store.get(tokenId, function(err, validSession) { 
+    if (err) {
+      console.log("Ocurrió un error al obtener la sesión guardada");
+      console.log(err);
+      res.send(401,{});
+    } else {
+      if (validSession) {
+        backendService(req, function(status, result) { res.status(status).send(result); } );
+      } else {
+        res.send(401,{});
+      }
+    }
+  });
 });
 
 /** 
@@ -49,10 +124,9 @@ app.post("/api/v1/autos/*", function (request, response) {
  */
 function backendService(request, onResult) {
   var path = request.url.substring(13); // -> /api/v1/autos/*
-  
   var options = {
-    host: host, 
-    port: 10039,
+    host: HOST, 
+    port: PORT,
     path: '/wps/PA_CEA-Admin/services' + path,
     method: request.method
   };
@@ -61,9 +135,10 @@ function backendService(request, onResult) {
   {
     var chunks = [];
 
-    if (res.statusCode != '200') 
+    if (res.statusCode != '200') {
       onResult('500', {'error': 'Error al consumir servicio de Backend'});
-    else {
+      console.log(res);
+    } else {
 
       res.on('data', function (chunk) {
         chunks.push(chunk);
@@ -93,192 +168,6 @@ function backendService(request, onResult) {
   if (request.method == 'POST' && request.data) req.write(querystring.stringify(request.data));
   req.end();
 };
-
-/** 
- * Autenticación con Backend
- * Victoria-Mobile-App -> Victoria-Mobile-App-Backend 
- * 
- * @param request: Solicitud desde Victoria-Mobile-App
- * @param response: Respuesta a Victoria-Mobile-App
- */
-app.get("/api/v1/login", function (request, response) {
-  var onResult = function(status, result) {
-      response.statusCode = status;
-      response.send(result);
-  };
-  
-  //var usuario = request.params.userid;
-  //var clave = request.params.password;
-  var usuario = request.query.userid;
-  var clave = request.query.password;
-  
-  
-  var options = {
-    host: host, 
-    port: 10039,
-    path: '/wps/portal/cxml/04_SD9ePMtCP1I800I_KydQvyHFUBADPmuQy?userid=' + usuario + '&password=' + clave,
-    method: 'GET'
-  };
-
-  var req = http.request(options, function(res)
-  {
-    var output;
-    console.log(options.host + ':' + res.statusCode);
-    
-    switch (res.statusCode) {
-    case 302:
-      res.statusCode = 200;
-      output = {"authenticated": true};
-      break;
-    case 404:
-      res.statusCode = 200;
-      output =  {"authenticated": false};
-      break;
-    default:
-      output = {"error": "Error al consumir servicio de Backend"};
-      break;
-    }
-
-    onResult(res.statusCode, output);
-  });
-
-  req.on('error', function(err) {
-    onResult('500', {'error': 'Error al consumir servicio de Backend'});
-  });
-
-  req.end();
-});
-
-// -------------------------- Unprotected mobile backend end ------------------------------
-
-// ------------ Protecting mobile backend with Mobile Client Access start -----------------
-
-
-// Load passport (http://passportjs.org)
-var passport = require('passport');
-
-// Get the MCA passport strategy to use
-//var MCABackendStrategy = require('bms-mca-token-validation-strategy').MCABackendStrategy; // package -> "bms-mca-token-validation-strategy": "^2.0.9"
-//var oauthSDK = require('bms-mca-oauth-sdk'); // package -> "bms-mca-oauth-sdk": "^3.0.3"
-const APIStrategy = require("bluemix-appid").APIStrategy; // package -> "bluemix-appid": "^1.0.3"
-
-//Tell application to use passport
-app.use(passport.initialize());
-
-// Tell passport to use the MCA strategy
-//passport.use(new MCABackendStrategy())
-// TODO Para prueba local
-passport.use(new APIStrategy({
-  tenantId: "a911b5ea-5f28-4e26-aa8a-6791f4a5020c",
-  oauthServerUrl: "https://appid-oauth.ng.bluemix.net/oauth/v3/a911b5ea-5f28-4e26-aa8a-6791f4a5020c"
-}));
-
-
-// Protect /protected endpoint which is used in Getting Started with Bluemix Mobile Services tutorials
-//app.get('/api/v1/login2', passport.authenticate('mca-backend-strategy', { session: true }), function(req, res) {
-//  res.send(200, req.securityContext);
-//});
-
-//Declare the API you want to protect
-app.get("/api/protected",
-    passport.authenticate(APIStrategy.STRATEGY_NAME, {
-        session: false
-    }),
-    function(req, res) {
-        // Get full appIdAuthorizationContext from request object
-        var appIdAuthContext = req.appIdAuthorizationContext;
- 
-        appIdAuthContext.accessToken; // Raw access_token
-        appIdAuthContext.accessTokenPayload; // Decoded access_token JSON
-        appIdAuthContext.identityToken; // Raw identity_token
-        appIdAuthContext.identityTokenPayload; // Decoded identity_token JSON
- 
-        // Or use user object provided by passport.js
-        var username = req.user.name || "Anonymous";
-        res.send("Hello from protected resource " + username);
-    }
-);
-
-app.get("/api/login",
-    passport.authenticate(APIStrategy.STRATEGY_NAME, {
-      session: true
-  }), function (request, response) {
-  var options = {cacheSize:1000};
-  
-  res.send("Logged in ");
-});
-
-
-/** 
- * Autenticación con Backend
- * Victoria-Mobile-App -> Victoria-Mobile-App-Backend 
- * 
- * @param request: Solicitud desde Victoria-Mobile-App
- * @param response: Respuesta a Victoria-Mobile-App
- */
-app.post("/api/v1/login", function (request, response) {
-  var onResult = function(status, result) {
-    response.statusCode = status;
-    response.send(result);
-  };
-  
-  if (!(request.headers.authorization && request.headers.authorization.indexOf("a911b5ea-5f28-4e26-aa8a-6791f4a5020c") > 0)) {
-    onResult("401", "Unauthorized");
-  } else {
-     
-    
-    var usuario = request.body.userid;
-    var clave = request.body.password;
-    
-    
-    var options = {
-      host: host, 
-      port: 10039,
-      path: '/wps/portal/cxml/04_SD9ePMtCP1I800I_KydQvyHFUBADPmuQy?userid=' + usuario + '&password=' + clave,
-      method: 'GET'
-    };
-  
-    var req = http.request(options, function(res)
-    {
-      var output;
-      console.log(options.host + ':' + res.statusCode);
-      
-      switch (res.statusCode) {
-      case 302:
-        res.statusCode = 200;
-        output = {"authenticated": true, "token": "mitoken"};
-        /*
-        var options = {cacheSize:1000};
-        oauthSDK.getAuthorizationHeaderBySecret(options).then(function(authHeader) {
-            output.token = authHeader;
-        }, function(err) {
-            console.log(err);
-            //TODO Eliminar propagación de error
-            output.token = err;
-        });
-        */
-        break;
-      case 404:
-        res.statusCode = 200;
-        output =  {"authenticated": false};
-        break;
-      default:
-        output = {"error": "Error al consumir servicio de Backend"};
-        break;
-      }
-  
-      onResult(res.statusCode, output);
-    });
-  
-    req.on('error', function(err) {
-      onResult('500', {'error': 'Error al consumir servicio de Backend'});
-    });
-  
-    req.end();
-  }
-});
-
-// ------------ Protecting backend APIs with Mobile Client Access end -----------------
 
 var port = process.env.PORT || 3000;
 
